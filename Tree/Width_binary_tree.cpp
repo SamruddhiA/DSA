c9f0cff5767d82e344f09b75ea35e12efa207a3c
Samruddhi <alekar.samruddhi@gmail.com>
2023-08-15 22:55:15 +0530

    Maximum width of binary tree

---
+++ Tree/Width_binary_tree.cpp
@@ -0,0 +1,63 @@
+// Maximum width of binary tree
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+
+class TreeNode
+{
+public:
+    T val;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int getMaxWidth(TreeNode<int> *root)
+{
+    // Write your code here.
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    queue<TreeNode<int> *> q;
+
+    q.push(root);
+
+    int res = 0;
+
+    while (!q.empty())
+    {
+
+        int count = q.size();
+
+        res = max(res, count);
+
+        for (int i = 0; i < count; i++)
+        {
+            TreeNode<int> *curr = q.front();
+            q.pop();
+
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+
+    return res;
+}
